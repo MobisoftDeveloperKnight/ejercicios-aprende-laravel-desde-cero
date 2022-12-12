c7479aab122cb5f3e2ae2caac6ead5734d7a5664
Oscar <oscar@mobisoft.works>
2022-12-12 11:23:17 +0100

    CRUD Show/Index

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
-
+Route::get('/', fn() => auth()->check() ? redirect('/home') : view('welcome'));
 
 Auth::routes();
 
@@ -30,6 +25,9 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 
 
 Route::get('/contacts/create', [ContactController::class, 'create'])->name('contacts.create');
+Route::post('/contacts/create', [ContactController::class, 'store'])->name('contacts.store');
+Route::get('/contacts/{contact}/', [ContactController::class, 'show'])->name('contacts.show');
 Route::get('/contacts/{contact}/edit', [ContactController::class, 'edit'])->name('contacts.edit');
 Route::put('/contacts/{contact}/', [ContactController::class, 'update'])->name('contacts.update');
-Route::post('/contacts/create', [ContactController::class, 'store'])->name('contacts.store');
+Route::delete('/contacts/{contact}/', [ContactController::class, 'destroy'])->name('contacts.destroy');
+Route::get('/contacts', [ContactController::class, 'index'])->name('contacts.index');
